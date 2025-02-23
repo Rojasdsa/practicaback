c0620d9c972578cd6b52e6236229e345f12921dc
Rojasdsa <rojasdsa@gmail.com>
2025-02-23 15:18:23 +0100

    2.0 - Vista welcome, header, navbar y footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="custom-footer text-center py-4">
+    <p class="mb-0">© 2025 Tejidos Violeta. Todos los derechos reservados.</p>
+</footer>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,57 @@
+{{-- NAVBAR responsive --}}
+<nav class="navbar navbar-expand-lg custom-nav py-0">
+    {{-- Centramos el nav --}}
+    <div class="container">
+        {{-- Logo + redirección a inicio --}}
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="https://pbs.twimg.com/profile_images/947504948765450240/nZtFY4Lb_400x400.jpg" class="rounded-circle custom-nav-logo">
+        </a>
+        {{-- Toggler para móviles --}}
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            {{-- Menú TIENDA --}}
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('/') ? 'custom-active-link' : '' }}" href="{{ url('/') }}">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('productos') ? 'custom-active-link' : '' }}"
+                        href="{{ url('/productos') }}">Productos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('pedidos') ? 'custom-active-link' : '' }}"
+                        href="{{ url('/pedidos') }}">Pedidos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ request()->is('clientes') ? 'custom-active-link' : '' }}"
+                        href="{{ url('/clientes') }}">Clientes</a>
+                </li>
+            </ul>
+            {{-- Menú USUARIO --}}
+            <ul class="navbar-nav ms-auto">
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                            @csrf
+                            <button type="submit" class="nav-link btn btn-link">Salir</button>
+                        </form>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Iniciar Sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
+                    </li>
+                @endauth
+            </ul>
+        </div>
+    </div>
+</nav>

0e9672c1e227f67e0f1a861e2121501c77441aff
Rojasdsa <rojasdsa@gmail.com>
2025-03-04 15:37:11 +0100

    API parte 2 (get, post, put, patch, delete)

--- app/Http/Controllers/retalController.php
+++ app/Http/Controllers/retalController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 
 class retalController extends Controller
 {
+    /* VER TODOS LOS RETALES */
     public function index()
     {
         $retales = Retal::all();
@@ -21,12 +22,34 @@ class retalController extends Controller
         return response()->json($data, 200);
     }
 
+    /* VER UN RETAL */
+    public function show($id)
+    {
+        $retal = Retal::find($id);
+
+        if (!$retal) {
+            $data = [
+                'message' => 'Retal no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $data = [
+            'retal' => $retal,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /* CREAR RETALES */
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'nombre' => 'required',
-            'metros' => 'required|digits:4',
-            'precio' => 'required|digits:4',
+            'metros' => 'required',
+            'precio' => 'required',
             'image' => 'required'
         ]);
 
@@ -58,5 +81,125 @@ class retalController extends Controller
             'retal' => $retal,
             'status' => 201
         ];
+    }
+
+    /* EDITAR DATOS DE UN RETAL */
+    public function update(Request $request, $id)
+    {
+        $retal = Retal::find($id);
+
+        if (!$retal) {
+            $data = [
+                'message' => 'Retal no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required',
+            'metros' => 'required',
+            'precio' => 'required',
+            'image' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        $retal->nombre = $request->nombre;
+        $retal->metros = $request->metros;
+        $retal->precio = $request->precio;
+        $retal->image = $request->image;
+
+        $retal->save();
+
+        $data = [
+            'message' => 'Retal actualizado',
+            'retal' => $retal,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /* EDITAR DATOS DE UN RETAL */
+    public function updatePartial(Request $request, $id)
+    {
+        $retal = Retal::find($id);
+
+        if (!$retal) {
+            $data = [
+                'message' => 'Retal no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nombre' => '',
+            'metros' => '',
+            'precio' => '',
+            'image' => ''
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'Error al validar datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        if ($request->has('nombre')) {
+            $retal->nombre = $request->nombre;
+        }
+        if ($request->has('metros')) {
+            $retal->metros = $request->metros;
+        }
+        if ($request->has('precio')) {
+            $retal->precio = $request->precio;
+        }
+        if ($request->has('image')) {
+            $retal->image = $request->image;
+        }
+
+        $retal->save();
+
+        $data = [
+            'message' => 'Retal actualizado',
+            'retal' => $retal,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+
+    /* ELIMINAR UN RETAL */
+    public function destroy($id)
+    {
+        $retal = Retal::find($id);
+
+        if (!$retal) {
+            $data = [
+                'message' => 'Retal no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $retal->delete();
+
+        $data = [
+            'message' => 'Retal eliminado',
+            'status' => 200
+        ];
+        return response()->json($data, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,16 @@ use App\Http\Controllers\retalController;
 
 Route::get('/retales', [retalController::class, 'index']);
 
+Route::get('/retales/{id}', [retalController::class, 'show'] );
+
 Route::post('/retales', [retalController::class, 'store']);
 
+Route::put('/retales/{id}', [retalController::class, 'update']);
+
+Route::patch('/retales/{id}', [retalController::class, 'updatePartial']);
+
+Route::delete('/retales/{id}', [retalController::class, 'destroy']);
 
 
 
-Route::get('/retales/{id}', function () {
-    return 'Un retal';
-});
 
-Route::put('/retales/{id}', function () {
-    return 'Actualizar retales';
-});
-Route::delete('/retales/{id}', function () {
-    return 'Eliminar retales';
-});
